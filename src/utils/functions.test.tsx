b9c775531026b6215457808a9753b815b73663f3
Felipe Soares <felipesoaresdemg@hotmail.com>
2019-05-17 14:57:29 -0300

    move functions test file

---
+++ src/utils/functions.test.tsx
@@ -0,0 +1,65 @@
+import fn from "./functions";
+
+it("remove registros duplicados de um vetor de objetos conforme o atributo informado", () => {
+  const data = [{ id: 1 }, { id: 2 }, { id: 1 }, { id: 3 }, { id: 3 }];
+  const attribute = "id";
+
+  expect(fn.getUnique(data, attribute)).toEqual([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 }
+  ]);
+});
+
+it("remove acentos de uma string", () => {
+  const data = "ÁéÃõÔôÇçÀà";
+  expect(fn.removeAccents(data)).toEqual("AeAoOoCcAa");
+});
+
+it("ordena um vetor de objetos conforme o atributo informado", () => {
+  const data = [
+    { name: "de" },
+    { name: "bc" },
+    { name: "a" },
+    { name: "cd" },
+    { name: "ab" }
+  ];
+  const attribute = "name";
+
+  expect(fn.sort(data, attribute)).toEqual([
+    { name: "a" },
+    { name: "ab" },
+    { name: "bc" },
+    { name: "cd" },
+    { name: "de" }
+  ]);
+});
+
+describe("filtra um vetor de objetos conforme o termo pesquisado", () => {
+  const data = [
+    { name: "de" },
+    { name: "bc" },
+    { name: "a" },
+    { name: "cd" },
+    { name: "ab" }
+  ];
+  const attribute = "name";
+
+  it("termo 'a'", () => {
+    let q = "a";
+    expect(fn.filter(q, data, attribute)).toEqual([
+      { name: "a" },
+      { name: "ab" }
+    ]);
+  });
+
+  it("termo 'bc'", () => {
+    let q = "bc";
+    expect(fn.filter(q, data, attribute)).toEqual([{ name: "bc" }]);
+  });
+
+  it("termo 'fg'", () => {
+    let q = "fg";
+    expect(fn.filter(q, data, attribute)).toEqual([]);
+  });
+});
